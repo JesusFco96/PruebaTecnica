cbc5cfc63514f1396b3383d16effb5c6f1bc5559
Jesus <xedinl30@gmail.com>
2020-01-21 13:28:22 -0600

    Subiendo Project

---
+++ pruebaTecnica/Controllers/PersonasController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using pruebaTecnica.Models;
+
+namespace pruebaTecnica.Controllers
+{
+    public class PersonasController : Controller
+    {
+        private PruebaBDEntities1 db = new PruebaBDEntities1();
+
+        // GET: Personas
+        public ActionResult Index()
+        {
+            return View(db.Personas.ToList());
+        }
+
+        // GET: Personas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Personas personas = db.Personas.Find(id);
+            if (personas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personas);
+        }
+
+        // GET: Personas/Create
+        public ActionResult Create()
+        {
+            List<SelectListItem> lst = new List<SelectListItem>();
+
+            lst.Add(new SelectListItem() { Text = "Hombre", Value = "Hombre" });
+            lst.Add(new SelectListItem() { Text = "Mujer", Value = "Mujer" });
+
+            ViewBag.Opciones = lst;
+
+            return View();
+        }
+
+        // POST: Personas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idPersonas,Nombres,ApPaterno,ApMaterno,FechaNacimiento,Sexo,EstadoNacimiento,Telefono,Direccion")] Personas personas)
+        {
+            if (ModelState.IsValid)
+            {
+
+                db.Personas.Add(personas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(personas);
+        }
+
+        // GET: Personas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Personas personas = db.Personas.Find(id);
+            if (personas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personas);
+        }
+
+        // POST: Personas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idPersonas,Nombres,ApPaterno,ApMaterno,FechaNacimiento,Sexo,EstadoNacimiento,Telefono,Direccion")] Personas personas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(personas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(personas);
+        }
+
+        // GET: Personas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Personas personas = db.Personas.Find(id);
+            if (personas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personas);
+        }
+
+        // POST: Personas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Personas personas = db.Personas.Find(id);
+            db.Personas.Remove(personas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
